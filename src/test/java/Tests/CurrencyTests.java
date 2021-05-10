eea520418ab0a80d7a0a71f7c572068f4fcc013a
Charris1904 <a.charris.bueno@gmail.com>
2021-05-09 18:38:25 -0600

    Update CurrencyTests.java

--- src/test/java/Tests/CurrencyTests.java
+++ src/test/java/Tests/CurrencyTests.java
@@ -1,6 +1,6 @@
 package Tests;
 
-import DataProvider.CurrencyDataProvider;
+import DataProvider.ProductDataProvider;
 import PageObjects.HomePage;
 import PageObjects.ProductPage;
 import PageObjects.SearchPage;
@@ -12,15 +12,16 @@ import static org.testng.AssertJUnit.assertTrue;
 public class CurrencyTests extends BaseTests {
 
     @Test(groups = {"currency"},
-            dataProvider = "getCurrencyDataFromJson",
-            dataProviderClass = CurrencyDataProvider.class)
+            dataProvider = "getProductsDataFromJson",
+            dataProviderClass = ProductDataProvider.class)
     public void testProductCurrency(Product product) {
-        HomePage homePage = new HomePage(driver);
-        SearchPage searchPage = homePage.searchProduct(product.getName());
-        ProductPage productPage = searchPage.clickProduct(product.getName());
+        HomePage homePage = new HomePage(driver, product.getName());
+        SearchPage searchPage = homePage.searchProduct();
+        ProductPage productPage = searchPage.clickProduct();
+        System.out.println("---------------- " + product.getName() + " ----------------");
         for (int i = 1; i <= 3; i++) {
-            System.out.println("---------------- " + product.getName() + " ----------------");
             String currency = productPage.selectCurrency(i);
+            System.out.println("Currency Symbol : " + currency);
             assertTrue(productPage.getProductPrice().contains(currency));
         }
     }
